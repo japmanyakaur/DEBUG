0847bd547d82c0ecd265f7c1bd60a75a435f2bcf
Manrai Singh <smanraisingh@email.com>
2026-02-03 19:09:38 +0530

    added database

--- backend/main.py
+++ backend/main.py
@@ -1,7 +1,24 @@
 from fastapi import FastAPI
 from pydantic import BaseModel
+from sqlalchemy import Column, Integer, String, Float
+from database import Base, engine, SessionLocal
+from models import LogDB
 
 app = FastAPI()
+
+Base.metadata.create_all(bind=engine)
+
+# Incident table
+
+
+class Incident(Base):
+    __tablename__ = "incidents"
+    __table_args__ = {"extend_existing": True}
+
+    id = Column(Integer, primary_key=True, index=True)
+    service_name = Column(String)
+    start_time = Column(Float)
+    status = Column(String)
 
 class Log(BaseModel):
     service_name: str
@@ -11,7 +28,40 @@ class Log(BaseModel):
     request_id: str
     endpoint: str
 
+
 @app.post("/logs")
 async def receive_log(log: Log):
-    print("LOG RECEIVED:", log)
+    db = SessionLocal()
+
+    db_log = LogDB(**log.dict())
+    db.add(db_log)
+    db.commit()
+
+    print("LOG SAVED TO DB")
+
+    # 🚨 Incident detection
+    if log.level == "ERROR":
+        recent_errors = db.query(LogDB).filter(
+            LogDB.service_name == log.service_name,
+            LogDB.level == "ERROR",
+            LogDB.timestamp >= log.timestamp - 60
+        ).count()
+
+        if recent_errors >= 3:
+            incident = Incident(
+                service_name=log.service_name,
+                start_time=log.timestamp,
+                status="ACTIVE"
+            )
+            db.add(incident)
+            db.commit()
+            print("🚨 INCIDENT CREATED")
+
     return {"status": "ok"}
+
+
+@app.get("/logs")
+def get_logs():
+    db = SessionLocal()
+    logs = db.query(LogDB).all()
+    return logs
